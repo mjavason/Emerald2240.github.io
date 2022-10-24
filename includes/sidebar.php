0cc3fa5e93f1fc88ed5926a3c6e6771b65ea4e59
Orji Micheal <orjimichael4886@gmail.com>
2022-10-24 20:32:21 +0100

    So many changes... copied in some code from user side

---
+++ includes/sidebar.php
@@ -0,0 +1,85 @@
+<div class="sidebar-main sidebar-menu-one sidebar-expand-md sidebar-color">
+    <div class="mobile-sidebar-header d-md-none">
+        <div class="header-logo">
+            <a href="index-2"><img src="img/logo1.png" alt="logo"></a>
+        </div>
+    </div>
+    <div class="sidebar-menu-content">
+        <ul class="nav nav-sidebar-menu sidebar-toggle-view">
+            <li class="nav-item sidebar-nav-item">
+                <a href="#" class="nav-link"><i class="flaticon-dashboard"></i><span>Dashboard</span></a>
+                <ul class="nav sub-group-menu">
+                    <li class="nav-item">
+                        <a href="index-2" class="nav-link"><i class="fas fa-angle-right"></i>Admin</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="index3" class="nav-link"><i class="fas fa-angle-right"></i>Students</a>
+                    </li>
+                </ul>
+            </li>
+            <li class="nav-item sidebar-nav-item">
+                <a href="#" class="nav-link"><i class="flaticon-classmates"></i><span>My Students</span></a>
+                <ul class="nav sub-group-menu">
+                    <li class="nav-item">
+                        <a href="all-student" class="nav-link"><i class="fas fa-angle-right"></i>All
+                            Students</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="student-details" class="nav-link"><i class="fas fa-angle-right"></i>Student Details</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="admit-form" class="nav-link"><i class="fas fa-angle-right"></i>Admission Form</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="student-promotion" class="nav-link"><i class="fas fa-angle-right"></i>Student Promotion</a>
+                    </li>
+                </ul>
+            </li>
+
+            <li class="nav-item sidebar-nav-item">
+                <a href="#" class="nav-link"><i class="flaticon-maths-class-materials-cross-of-a-pencil-and-a-ruler"></i><span>Class</span></a>
+                <ul class="nav sub-group-menu">
+                    <li class="nav-item">
+                        <a href="all-class" class="nav-link"><i class="fas fa-angle-right"></i>All
+                            Classes</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="add-class" class="nav-link"><i class="fas fa-angle-right"></i>Add New
+                            Class</a>
+                    </li>
+                </ul>
+            </li>
+            <li class="nav-item">
+                <a href="all-subject" class="nav-link"><i class="flaticon-open-book"></i><span>My Subjects</span></a>
+            </li>
+           
+            <li class="nav-item">
+                <a href="student-attendence" class="nav-link"><i class="flaticon-checklist"></i><span>Attendence</span></a>
+            </li>
+            <li class="nav-item sidebar-nav-item">
+                <a href="#" class="nav-link"><i class="flaticon-shopping-list"></i><span>Exam</span></a>
+                <ul class="nav sub-group-menu">
+                    <li class="nav-item">
+                        <a href="exam-schedule" class="nav-link"><i class="fas fa-angle-right"></i>Exam
+                            Schedule</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="exam-grade" class="nav-link"><i class="fas fa-angle-right"></i>Exam
+                            Grades</a>
+                    </li>
+                </ul>
+            </li>
+           
+           
+            <li class="nav-item">
+                <a href="notice-board" class="nav-link"><i class="flaticon-script"></i><span>Notice</span></a>
+            </li>
+
+            
+           
+            <li class="nav-item">
+                <a href="account-settings" class="nav-link"><i class="flaticon-settings"></i><span>Account</span></a>
+            </li>
+        </ul>
+    </div>
+</div>
